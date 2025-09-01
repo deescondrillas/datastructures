fb9b35d4814022776a4ac3efcd9381675af6457e
Franco De E <escondrillas.franco@gmail.com>
2025-09-01 15:10:31 -0600

    Update act1.cpp

--- act1.cpp
+++ act1.cpp
@@ -8,7 +8,7 @@
 
 using namespace std;
 
-// definiciones
+// Definiciones
 typedef unsigned long long int lli;
 
 // Calcula la sumatoria de 1 hasta n con un metodo iterativo
@@ -22,16 +22,16 @@ lli sumaDirecta(lli n);     // O(1)
 
 
 int main() {
-    // input – entero positivo (n)
+    // Input – entero positivo (n)
     lli n; cin >> n;
-    // output – Σ de 1 hasta n
+    // Output – Σ de 1 hasta n
     cout << sumaIterativa(n) << endl;
     cout << sumaRecursiva(n) << endl;
     cout << sumaDirecta(n) << endl;
     return 0;
 }
 
-// funciones
+// Funciones
 lli sumaIterativa(lli n) {
     lli out = 0;
     for(lli i = 1; i <= n; ++i) out += i;
