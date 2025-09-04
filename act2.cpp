50ac804a242f4219814e66dbdf702e3d5d57a029
Franco De E <escondrillas.franco@gmail.com>
2025-09-04 16:50:30 -0600

    Create act2.cpp

---
+++ act2.cpp
@@ -0,0 +1,21 @@
+// Act 1.2 – 10 de septiembre, 2025
+
+// A01739755 – Michelle Amanda Hernández Legaria
+// A01739410 – Franco De Escondrillas Vázquez
+// A01739511 – Jorge Luis Zago Guevara
+
+#include <iostream>
+
+using namespace std;
+
+// Definiciones
+typedef unsigned long long int lli;
+
+// Ordena en forma ascendente los datos con el método de Intercambio
+void swapsort(lli n);   // O(n²)
+
+// Ordena en forma ascendentelos datos con el método de Burbuja
+void bubblesort(lli n);   // O(n²)
+
+// Ordena en forma ascendente los datos con el método de Merge
+void mergesort(lli n);     // O(…)
