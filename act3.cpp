c8451882a69dc009dbede192dc288f96566a1155
Franco De E <escondrillas.franco@gmail.com>
2025-09-11 15:51:28 -0600

    Create act3.cpp

---
+++ act3.cpp
@@ -0,0 +1,29 @@
+// Act 1.3 – 14 de septiembre, 2025
+
+// A01739755 – Michelle Amanda Hernández Legaria
+// A01739410 – Franco De Escondrillas Vázquez
+// A01739511 – Jorge Luis Zago Guevara
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// Definiciones
+#define rp(i, a, b) for(int i = (a), TT = (b); i < TT; ++i)                         // Definición de un ciclo for rápido
+#define iostream_testcases ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);     // Gestiona todos los cin primero, y luego los cout
+
+// Ordena en forma ascendente los datos con el método de Merge
+void ordenaMerge(int, int*);        // O(n log₂n)
+
+int main() {
+    iostream_testcases
+    // ...
+    cout << "En proceso" << endl;
+    return 0;
+}
+
+// Funciones
+void ordenaMerge(int sz, int *arr) {
+    // ...
+}
