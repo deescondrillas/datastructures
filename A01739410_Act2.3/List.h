04d05799e7723b73f2bdf2e3e5d12b4d4721faf2
Franco De E <escondrillas.franco@gmail.com>
2025-10-13 17:57:53 -0600

    Update List.h

--- A01739410_Act2.3/List.h
+++ A01739410_Act2.3/List.h
@@ -15,12 +15,92 @@ class List {
         List() {
 
         }
+        List(Node<T>* _head) {
+            head = _head;
+        }
         // Inserta elementos al final | O(1)
         void insert(T value) {
             Node<T>* newNode = new Node<T>(value, tail, NULL);
             if(!head) head = newNode;
             tail = newNode;
         }
+        // Ordenar ascendentemente | O(n log₂n)
+        void mergeSort() {
+            //condicion base
+            if (head == tail) return;
+
+            Node<T>* slowP = head;
+            Node<T>* fastP = head;
+
+            //Recorrido a la mitad O(n)
+            while(fastP -> next) {
+                fastP = fastP -> next;
+                slowP = slowP -> next;
+                if(fastP -> next) fastP = fastP -> next;
+            }
+
+            // Separar listas
+            slowP -> prev -> next = NULL;
+            slowP -> prev = NULL;
+            List<T> newList(slowP);
+
+            // Llamada recursiva
+            newList.mergeSort();
+            mergeSort();
+
+            // Unir listas
+            Node<T> *ptL1(head), *ptL2(slowP);
+            head = tail = NULL;
+            while(ptL1 && ptL2) {
+                // Insertar primer elemento
+                if(ptL1->data < ptL2->data) {
+                    // Lista vacia
+                    if(!head) {
+                        head = ptL1;
+                        tail = ptL1;
+                    }
+                    // Lista con elementos
+                    else {
+                        tail->next = ptL1;
+                        ptL1->prev = tail;
+                        tail = tail->next;
+                    }
+                        ptL1 = ptL1->next;
+                }
+                else {
+                     // Lista vacia
+                    if(!head) {
+                        head = ptL2;
+                        tail = ptL2;
+                    }
+                    // Lista con elementos
+                    else {
+                        tail->next = ptL2;
+                        ptL1->prev = tail;
+
+                        tail = tail -> next;
+                    }
+                        ptL2 = ptL2->next;
+                }
+            }
+            // Insertar lista sobrante
+            if(ptL1) {
+                tail->next = ptL1;
+                ptL1->prev = tail;
+                tail = tail->next;
+            } else {
+                tail->next = ptL2;
+                ptL2->prev = tail;
+                tail = tail->next;
+            }
+        }
+
+        // Busqueda lineal | O(n)
+        string search(string IP1, string IP2) {
+            int ip1[4], ip2[4];
+            return 'a';
+        }
+
         // Imprime los elementos de la lista | O(n)
         string print() {
             string out = "";
