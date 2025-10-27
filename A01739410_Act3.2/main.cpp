ef2bad02ead3e2a20ef78730fa244c0506c0f55a
Franco De E <escondrillas.franco@gmail.com>
2025-10-26 19:18:24 -0600

    pop() & front() comments

--- A01739410_Act3.2/main.cpp
+++ A01739410_Act3.2/main.cpp
@@ -16,25 +16,28 @@ int main() {
     while(option) {
         cin >> option;
         switch(option) {
-            case 1:
+            case 1:                 // push  | O(log₂n)
                 cin >> value;
                 heap.push(value);
                 break;
-            case 2:
-                cout << heap.top() << endl;
+            case 2:                 // pop   | O(log₂n)
+                // Para que el output coincida con el ejemplo de salida,
+                // cuando se hace un pop() tambien se debe imprimir el
+                // elemento que se saca, aunque la tarea no lo indica
+                cout << heap.top() << endl; // Esta es la linea adicional
                 heap.pop();
                 break;
-            case 3:
+            case 3:                 // print | O(n)
                 heap.print();
                 break;
-            case 4:
+            case 4:                 // top   | O(1)
                 cout << heap.top() << endl;
                 break;
-            case 5:
+            case 5:                 // empty | O(1)
                 if(heap.empty()) cout << "true" << endl;
                 else cout << "false" << endl;
                 break;
-            case 6:
+            case 6:                 // size  | O(1)
                 cout << heap.size() << endl;
                 break;
             default:
