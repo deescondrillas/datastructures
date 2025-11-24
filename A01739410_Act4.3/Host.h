9cae260c755dc61d14b74b8684e34f52bc90d73a
Sergio Sebastián Cortez Yepez <seboscortes@hotmail.com>
2025-11-23 20:09:01 -0600

    4.3 Avance en Red y Host

--- A01739410_Act4.3/Host.h
+++ A01739410_Act4.3/Host.h
@@ -1,9 +1,27 @@
 // Act 4.3 – 23 de noviembre, 2025
 // Clase Node
-
 #pragma once
 
 template <class T>
 class Host {
     // Clase para guardar la IP del host
+    private:
+        int ip3 = -1;
+        int ip4 = -1;
+        T *logs = nullptr;
+    public:
+    Host(int size = 1024){
+        this->logs = new T[size]();
+    }
+    
+    bool operator > (const Host<T>& otherHost) const {
+        return ip3 > otherHost.ip3;
+        if(ip3 == otherHost.ip3) return ip4 > otherHost.ip4;
+    }
+
+    bool operator < (const Host<T>& otherHost) const {
+        return ip3 < otherHost.ip3;
+        if(ip3 == otherHost.ip3) return ip4 < otherHost.ip4;
+    }
+    
 };
--- A01739410_Act4.3/Red.h
+++ A01739410_Act4.3/Red.h
@@ -6,4 +6,23 @@
 template <class T>
 class Red {
     // Clase para guardar la direccion de red
+    private:
+        int ip1 = -1;
+        int ip2 = -1;
+        T *hosts = nullptr;
+    public:
+    Red(int size = 1024){
+        this->hosts = new T[size]();
+    }
+
+    bool operator > (const Red<T>& otherRed) const {
+        return ip1 > otherRed.ip1;
+        if(ip1 == otherRed.ip1) return ip2 > otherRed.ip2;
+    }
+
+    bool operator < (const Red<T>& otherRed) const {
+        return ip1 < otherRed.ip1;
+        if(ip1 == otherRed.ip1) return ip2 < otherRed.ip2;
+    }
+    
 };
