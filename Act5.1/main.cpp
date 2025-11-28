6ac9b5a070153bf2aac76d32045f09675757c363
Franco De E <escondrillas.franco@gmail.com>
2025-11-28 13:24:22 -0600

    hash.search debug

--- Act5.1/main.cpp
+++ Act5.1/main.cpp
@@ -31,7 +31,7 @@ int main() {
                     break;
                 case 4: // search() – sin colision O(1) || WCS O(n)
                     cin >> key;
-                    cout << *hash.search(key) << endl;
+                    hash.search(key) ? cout << *hash.search(key) << endl : cout << "NULL" << endl;
                     break;
             }
         }
