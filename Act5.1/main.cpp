16253f463be6ba4f7116a0bf1653e39035dde5ef
Franco De E <escondrillas.franco@gmail.com>
2025-11-28 12:59:10 -0600

    Update main.cpp

--- Act5.1/main.cpp
+++ Act5.1/main.cpp
@@ -1,8 +1,8 @@
 // Act 5.1 – 29 de noviembre, 2025
 
 // A01739410 – Franco De Escondrillas Vazquez
-// A0173
-// A0173
+// A01739522 – Sergio Sebastian Cortez Yepez
+// A01739190 – Carlos Arturo Ferat Torres
 
 #include "Hash.h"
 
@@ -19,17 +19,17 @@ int main() {
         while(option) {
             cin >> option;
             switch(option) {
-                case 1:
+                case 1: // ins() – sin colision O(1) || WCS O(n)
                     cin >> nuevo;
                     hash.ins(nuevo);
                     break;
-                case 2:
+                case 2: // del() – sin colision O(1) || WCS O(n)
                     hash.del();
                     break;
-                case 3:
+                case 3: // print() | O(n)
                     hash.print();
                     break;
-                case 4:
+                case 4: // search() – sin colision O(1) || WCS O(n)
                     cin >> key;
                     cout << *hash.search(key) << endl;
                     break;
